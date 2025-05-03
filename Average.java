ed4377f84c7bf66de24a0a7d9335f32b63ebf16d
AngelJim0408 <jimeneza0908@gmail.com>
2025-05-03 17:38:11 -0500

    Update Average.java

--- Average.java
+++ Average.java
@@ -1,12 +1,15 @@
+package com.averageTest;
+
 public class Average {
-     public int average(int k, int[] list){
-                  int average = 0;
-                  int n = Math.min(k, list.length);
-                  if( n > 0) {
-                              for(int i = 0 ; i < n; i++){
-                                          average += list[i];
-                              }
-                              average = average/n;
-                  }
-                  return average;
-      }}
+
+    public int average(int k, int[] list){
+                 int average = 0;
+                 int n = Math.min(k, list.length);
+                 if( n > 0) {
+                             for(int i = 0 ; i < n; i++){
+                                         average += list[i];
+                             }
+                             average = average/n;
+                 }
+                 return average;
+    }}
